17a9f890f9aa97b195425c5008e14d72e99fa73f
Barchil Said <barchil@gmail.com>
2024-07-31 20:55:26 -0400

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,4 +1,7 @@
 module.exports = {
+    // put your own keys. Those are used for login using : Google, Facebook, Github. also
+    // contain the link to mongodb database hosted in mongodb Atlas.the last one is for
+    // cookies encryption.
     googleClientID: "",
     googleClientSecret:"", 
     facebookClientID: "", 
